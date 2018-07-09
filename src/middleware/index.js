36a0dadfb2ad0d13bc13d55dbda3b68c7139c92f
James Yarnell <jamesy@bajaconsulting.com>
2018-07-09 13:20:15 -0500

    added redux stuff

---
+++ src/middleware/index.js
@@ -0,0 +1,8 @@
+import thunk from 'redux-thunk';
+import {applyMiddleware} from 'redux';
+
+const middleware = [
+    thunk
+]
+
+export default () => applyMiddleware(...middleware);
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import {createStore} from 'redux';
+import rootReducer from './reducers';
+import middleware from './middleware';
+
+const store = createStore(rootReducer, middleware());
+export default store;
