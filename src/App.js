1eb8aa6cd853a0ad4b5b41c502ad9cdb8212be4c
Nicholas Teal <nteal15@gmail.com>
2018-07-10 08:59:40 -0500

    feature work on auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getRequestToken, pocketAuth } from './services/pocketServices';
+import {connect} from 'react-redux';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.signIn = this.signIn.bind(this);
+  }
+
+  signIn() {
+    const setState = this.setState.bind(this);
+    getRequestToken()
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.code);
+        setState({'code' : res.code});
+        window.open(pocketAuth(res.code))
+      })
+      .catch(e => console.log(e));
+  }
   render() {
     return (
       <div className="App">
-      hi -- doug is awesome
+      <button onClick={this.signIn}>sign in</button>
       </div>
     );
   }
 }
 
-export default App;
+mapDispatchToProps = function()
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/services/pocketServices.js
@@ -0,0 +1,37 @@
+const redirect_uri = process.env.REDIRECT_URI || 'http://localhost:3000'
+
+function enableCors(options) {
+  const cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+  return fetch(cors_api_url + options.url, {
+    method: options.method,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'X-Accept': 'application/json'
+  },
+    body: options.data,
+  })
+}
+
+export function getRequestToken() {
+  const options = {
+    url: 'https://getpocket.com/v3/oauth/request',
+    method: 'POST',
+    data: JSON.stringify({
+      'consumer_key':'78456-3d31f4a129b5a88b18473ccf',
+      'redirect_uri': redirect_uri
+    })
+  }
+  return enableCors(options);
+}
+
+export function pocketAuth(request_token) {
+  return `https://getpocket.com/auth/authorize?request_token=${request_token}&redirect_uri=${redirect_uri}`
+}
+
+export function getPocketAccessToken(consumer_key, ) {
+  
+}
+
+// const fetchPocketService = () {
+
+// }
