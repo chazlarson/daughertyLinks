8aed4258f1b3dd29006a74c305d0a5153d4f17d2
Doug Coleman <douglas.coleman@daugherty.com>
2018-07-10 09:07:37 -0500

    Removed James' Test Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Wrapper from './components/Wrapper.js'
-
-class App extends Component {
-=======
+import Wrapper from './components/Wrapper.js';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getDaughertyLinks } from './actions/daugherty-links.actions';
@@ -21,28 +17,14 @@ class App extends Component {
   componentWillMount(){
     this.props.getDaughertyLinks();
   }
->>>>>>> 4bb20a4f4509d11a57b67c6aa9fe87d7475aa014
 
   render() {
-    const links = this.props.daughertyLinks.items.map(item => {
-      return (
-        <div key={item.id}>
-          {item.title}
-        </div>
-      )
-    });
-
     return (
       <div className="App">
-<<<<<<< HEAD
         <Wrapper />
-=======
-      hi -- doug is awesome
-      {links}
->>>>>>> 4bb20a4f4509d11a57b67c6aa9fe87d7475aa014
       </div>
     );
   }
-}
+} 
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
