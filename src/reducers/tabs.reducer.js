71958194dd9a9400b359da1a6f69437881ad8eae
James Yarnell <jamesy@bajaconsulting.com>
2018-07-11 12:56:51 -0500

    added status message

--- src/reducers/tabs.reducer.js
+++ src/reducers/tabs.reducer.js
@@ -1,14 +1,18 @@
 import * as tabsActions from '../actions/tabs.action'
 
 const defaultState = {
-    selectedTab: "Daugherty"
+    selectedTab: "Daugherty",
+    statusMessage: ""
 };
 
 const tabsReducer = (state = defaultState, action) => {
     switch (action.type){
         case tabsActions.TAB_HAS_CHANGED:{
-            console.log('Selected Tab Changed: ', action.payload);
             state = {...state, selectedTab: action.payload}
+            break;
+        }
+        case tabsActions.STATUS_MESSAGE_CHANGED:{
+            state = {...state, statusMessage: action.payload}
             break;
         }
         default:{
