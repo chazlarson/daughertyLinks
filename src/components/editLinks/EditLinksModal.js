dad9cd5d1d80828a44ed48c631d7eacd3e395c71
Nicholas Teal <nteal15@gmail.com>
2018-07-23 18:33:35 -0500

    (cleanup)

--- src/components/editLinks/EditLinksModal.js
+++ src/components/editLinks/EditLinksModal.js
@@ -2,11 +2,10 @@ import React from 'react';
 import EditLinksTable from './EditLinksTable';
 import {withRouter} from 'react-router';
 
-// TODO: fix from being modal to being page
 const EditLinksModal = (props) => {
     return (
       <div className={'edit-links-modal'}>
-        <i className={'fa-2x clickable fa fa-arrow-circle-o-left green'} onClick={() => this.props.history.goBack()} title="back"></i>
+        <i className={'fa-2x clickable fa fa-arrow-circle-o-left green'} onClick={() => props.history.goBack()} title="back"></i>
         <EditLinksTable {...props}/>
       </div>)
   }
